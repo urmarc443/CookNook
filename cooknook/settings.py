117cdef1fd672af9acb925a3c5f4266867d353cc
urmarc443 <urmarc443@gmail.com>
2024-10-02 22:00:40 -0700

    REMOVE .get method from os.environ

--- cooknook/settings.py
+++ cooknook/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
-from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cooknook',
         'USER': 'root',
-        'PASSWORD': os.environ.get['DBPASS'],
+        'PASSWORD': os.environ['DBPASS'],
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
