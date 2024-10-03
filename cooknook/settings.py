87b3c6baacc104125c3685cd80f84fc40ef8d278
urmarc443 <urmarc443@gmail.com>
2024-10-02 19:15:55 -0700

    FIX os.envron.get["DBPASS"] and added wildcard to .gitignore

--- cooknook/settings.py
+++ cooknook/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cooknook',
         'USER': 'root',
-        'PASSWORD': os.environ['DBPASS'],
+        'PASSWORD': os.environ.get['DBPASS'],
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
